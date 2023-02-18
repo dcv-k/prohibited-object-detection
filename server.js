90b9a65d7aa9dcf464264a7fb6a0d55c011017db
chandrabhanu <dcv.kalansooriya@gmail.com>
2023-02-18 20:47:47 +0530

    new interfaces and server

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ app.listen(3000, () => {
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/', (req, res) => {
+    res.send('<h1>why r u gae</h1>')
+})
+
 app.post('/login', (req, res) => {
-    console.log(req.body)
     res.send({"msg": "1"})
 })
