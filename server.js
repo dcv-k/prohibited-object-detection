828153fcc11d7526127633c2c67c752937d2b828
chandrabhanu <dcv.kalansooriya@gmail.com>
2023-02-21 04:26:50 +0530

    login functions

--- server.js
+++ server.js
@@ -1,20 +1,39 @@
 const express = require("express")
 const cors = require("cors")
+const mysql = require("mysql2")
 const app = express()
 
-app.listen(3000, () => {
+app.use(express.json());
 
+let connection = undefined
+
+app.listen(3000, () => {
+     connection = mysql.createConnection({
+        host: "localhost",
+        port: "3306",
+        user: "root",
+        password: "",
+        database: "prohibited_object_detection"
+    })
+
+    if (connection) {
+        console.log("connection made")
+    }
 })
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    console.log(req.params)
-    res.send('<h1>why r u gae</h1>')
+    
 })
 
 app.post('/login', (req, res) => {
-    console.log(req.body)
-    res.send({"msg": "1"})
+    console.log(req.body.username, req.body.password)
+    connection.execute("SELECT * FROM users WHERE username = ? AND password = ?",
+                        [req.body.username, req.body.password],
+                        (err, results, fields) => {
+                            if (results[0])
+                                res.send({"idUsers": results[0].idUsers})  
+                        })
 })
